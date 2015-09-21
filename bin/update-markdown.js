f303fdb535b03048f08b6a2f3b17d19b602e4bea
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-20 22:13:24 -0400

    started cli

--- bin/update-markdown.js
+++ bin/update-markdown.js
@@ -1,2 +1,35 @@
 #!/usr/bin/env node
 
+var updateMarkdown = require('..');
+var read = require('fs').readFileSync;
+
+function getCliOptions(argv) {
+  switch (argv.length) {
+    case 2:
+      console.error('um <filename.md> <section header>');
+      console.error('for example: um some-file.md "## foo"')
+      console.error('updates section with title "## foo" inside file "some-filemd"');
+      process.exit(-1);
+    break;
+    case 4:
+      return {
+        filename: process.argv[2],
+        title: process.argv[3]
+      };
+    break;
+    case 5:
+      return {
+        filename: process.argv[2],
+        title: process.argv[3],
+        text: read(process.argv[4])
+      };
+    break;
+    default:
+      console.error('Do not know how to handle arguments');
+      console.error(argv.slice(2));
+      process.exit(-1);
+  }
+}
+
+var options = getCliOptions(process.argv);
+updateMarkdown(options);
---
+++ index.js
@@ -0,0 +1,7 @@
+var log = require('debug')('um');
+
+function updateMarkdown(options) {
+  options = options || {};
+}
+
+module.exports = updateMarkdown;
